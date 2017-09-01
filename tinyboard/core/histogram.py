1032e44a7d0e3ce71b2dc46fc30720222b2b42de
Danqing Liu <danqing@awwfish.com>
2017-09-01 16:00:58 -0700

    Clean up some docs

--- tinyboard/core/histogram.py
+++ tinyboard/core/histogram.py
@@ -7,26 +7,17 @@ from tinyboard.proto.summary_pb2 import HistogramProto
 from tinyboard.proto.summary_pb2 import Summary
 
 
-def histogram(name, values, bins, collections=None):
-    # pylint: disable=line-too-long
-    """Outputs a `Summary` protocol buffer with a histogram.
-    The generated
-    [`Summary`](https://www.tensorflow.org/code/tensorflow/core/framework/summary.proto)
-    has one summary value containing a histogram for `values`.
-    This op reports an `InvalidArgument` error if any value is not finite.
-    Args:
-      name: A name for the generated node. Will also serve as a series name in
-        TensorBoard.
-      values: A real numeric `Tensor`. Any shape. Values to use to
-        build the histogram.
-      collections: Optional list of graph collections keys. The new summary op is
-        added to these collections. Defaults to `[GraphKeys.SUMMARIES]`.
-    Returns:
-      A scalar `Tensor` of type `string`. The serialized `Summary` protocol
-      buffer.
+def histogram(name, tensor, bins, collections=None):
+    """Prepare a summary proto containing a histogram.
+
+    :param string name: The name of the histogram.
+    :param Tensor tensor: The tensor of the histogram. Can be of any shape.
+    :param list<string> collections: Optional collections keys. The new summary
+        will be added to these collections.
+    :returns Summary: The summary proto containing the histogram.
     """
     name = clean_tag(name)
-    hist = make_histogram(values.astype(float), bins)
+    hist = make_histogram(tensor.astype(float), bins)
     return Summary(value=[Summary.Value(tag=name, histo=hist)])
 
 
